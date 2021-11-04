29309daaee93074643aabb0cb8dd03da87c47b05
mw1719 <mw1719@skbl-422-010.ph.ic.ac.uk>
2021-11-04 11:09:57 +0000

    increment counter with variable upperbound, clock cycle counting comments

--- main.s
+++ main.s
@@ -9,16 +9,20 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
+	movwf	TRISC, A	    ; Port C all outputs
+	movlw	0xFF
+	movwf	TRISD, A
+	movlw	0x0
+    	bra 	test
 loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
+	movff 	0x06, PORTC	    ; 2c
+	incf 	0x06, W, A	    ; 1c
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
+	movwf	0x06, A		    ; 1c, Test for end of loop condition
+	movf	PORTD, W, A	    ; 1c
+	cpfsgt 	0x06, A		    ; 1c
+	bra 	loop		    ; 2c, Not yet finished goto start of loop again
+				    ; 8c total, 4x8 = 32 clock cycles, 5e-7s for 1 loop
 	goto 	0x0		    ; Re-run program from start
 
 	end	main
