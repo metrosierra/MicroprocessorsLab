add18808de86108c94ba620062d713821332b35e
jz2319 <jz2319@skbl-422-010.ph.ic.ac.uk>
2021-11-16 11:53:41 +0000

    still buggy, cannot add numbers

--- main.s
+++ main.s
@@ -1,86 +1,59 @@
-#include <xc.inc>
+	#include <xc.inc>
 
-extrn UART_Setup, UART_Transmit_Message 	; external subroutines
-extrn LCD_Setup, LCD_Write_Message, LCD_Write_Instruction
-
-psect udata_acs		; reserve data space in access ram
-counter: ds 1		; reserve one byte for a counter variable
-delay_count:ds 1 	; reserve one byte for counter in the delay routine
-
-psect udata_bank4 	; reserve data anywhere in RAM (here at 0x400)
-myArray: ds 0x80 	; reserve 128 bytes for message data
-
-
-
-psect data
-; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db 'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-	; message, plus carriage return
-	myTable_l EQU 13 	; length of data
-	align 2
-
-psect code, abs
-rst: 
-	org	0x0
+    
+    
+psect   code, abs
+      
+main:
+	org 0x0
 	goto	setup
-
-
-
-; ******* Programme FLASH read Setup Code ***********************
-setup: 
-	bcf	CFGS 	; point to Flash program memory
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup ; setup UART
-	call	LCD_Setup 	; setup UART
-	goto	start
-
-; ******* Main programme ****************************************
 	
-clear:
-	movlw	00000001B
-	call	LCD_Transmit_Instruction
-	return
+	org 0x100
+
+setup: 
+	
+    
+    
+ 
+	banksel	PADCFG1	; enter bank with PADCFG1
+	bsf	REPU 	; point to Flash program memory with bit 1
+	banksel	0	; come back out to access bank
+	
+	clrf	LATE
+	
+	
+	
+	movlw	0xFF
+	movwf	0x02 ; for delay
+	call	delay
+	
+	
 	
 start:
-	lfsr	0, myArray 	; Load FSR0 with address in RAM
-	movlw	low highword(myTable) ; address of data in PM
-	movwf	TBLPTRU, A 	; load upper bits to TBLPTRU
-	movlw	high(myTable) 	; address of data in PM
-	movwf	TBLPTRH, A 	; load high byte to TBLPTRH
-	movlw	low(myTable) 	; address of data in PM
-	movwf	TBLPTRL, A 	; load low byte to TBLPTRL
-	movlw	myTable_l 	; bytes to read
-	movwf	counter, A 	; our counter register
+	nop
+
+loop:
+	movlw	00001111B; binary instead	;output=0 input=1
+	movwf	TRISE, A
 	
-loop: 
-	tblrd*+ 		; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move data from TABLAT to (FSR0), inc FSR0
-	decfsz	counter, A 	; count down to zero
-	bra	loop 		; keep going until finished
-
-	movlw	myTable_l 	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
-
-	movlw	11000000B	; position address instruction	
-	call	LCD_Write_Instruction
-
-	movlw	myTable_l 	; output message to LCD
-	addlw	0xff 		; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
-
-	call	clear
-	goto	$ 			; goto current line in code
-
-
-
-	; a delay subroutine if you need one, times around loop in delay_count
+	movff	PORTE, 0x06, A	; read all bits of PORTE
+				; keypad -> PORTE  ; 0-3 rows  ; 4-7 columns
+	
+	movlw	11110000B  ; binary instead	;output=0 input=1
+	movwf	TRISE, A    
+	
+	movff	PORTE, 0x07, A
+	
+	movf	0x06, W, A
+	addwfc	0x07, 0x08, A
+		
+	bra	loop
+	
+    
 delay: 
 
-	decfsz	delay_count, A 	; decrement until zero
+	decfsz	0x02, A 	; decrement until zero
 	bra	delay
 	return
-
-end rst
+	
+	end	main
