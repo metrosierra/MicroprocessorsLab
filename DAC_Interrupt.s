d3d34f39371f9d69fb205c255b91d8b9145703ed
jz2319 <jz2319@skbl-422-010.ph.ic.ac.uk>
2021-11-23 12:08:20 +0000

    dac works

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -7,14 +7,23 @@ psect	dac_code, class=CODE
 DAC_Int_Hi:	
 	btfss	TMR0IF		; check that this is timer0 interrupt
 	retfie	f		; if not then return
-	incf	LATJ, F, A	; increment PORTD
+	incf	LATJ, F, A	; increment PORTJ
+	
+	movlw	0x00
+	movwf	PORTD, A
+        nop
+	nop
 	bcf	TMR0IF		; clear interrupt flag
+	movlw	0x01
+	movwf	PORTD, A
+
+
 	retfie	f		; fast return from interrupt
 
 DAC_Setup:
-	clrf	TRISJ, A	; Set PORTD as all outputs
-	clrf	LATJ, A		; Clear PORTD outputs
-	movlw	10000111B	; Set timer0 to 16-bit, Fosc/4/256
+	clrf	TRISJ, A	; Set PORTJ as all outputs
+	clrf	LATJ, A		; Clear PORTJ outputs
+	movlw	10000000B	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON, A	; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	TMR0IE		; Enable timer0 interrupt
 	bsf	GIE		; Enable all interrupts
--- main.s
+++ main.s
@@ -4,12 +4,18 @@ extrn	DAC_Setup, DAC_Int_Hi
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
+	
+	movlw	0x00
+	movwf	TRISD, A
+	
 	goto	start
 
 int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
-start:	call	DAC_Setup
+start:	
+
+	call	DAC_Setup
 	goto	$	; Sit in infinite loop
 
 	end	rst
